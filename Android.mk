b155ab82a6cdb6309b9d86e11bfef5bec8e684ae
Nick Reuter <nreuter85@gmail.com>
2014-12-03 21:45:58 -0600

    invensense: allow other devices to build a specific sensor
    Conflicts:
    	Android.mk
    
    Change-Id: I4768013fdbd1a5a07e186a38b0a08a01dfa85594

--- Android.mk
+++ Android.mk
@@ -1,15 +1,19 @@
 # Can't have both 65xx and 60xx sensors.
 ifneq ($(filter hammerhead, $(TARGET_DEVICE)),)
 # hammerhead expects 65xx sensors.
-include $(call all-named-subdir-makefiles,65xx)
+TARGET_INVENSENSE_SENSOR := 65xx
 else
 ifneq ($(filter guppy dory, $(TARGET_DEVICE)),)
 # dory and guppy expect 6515 sensors.
-include $(call all-named-subdir-makefiles,6515)
+TARGET_INVENSENSE_SENSOR := 6515
 else
 ifneq ($(filter manta grouper tuna maguro toro toroplus mako, $(TARGET_DEVICE)),)
 # manta, grouper, tuna, and mako expect 60xx sensors.
-include $(call all-named-subdir-makefiles,60xx)
+TARGET_INVENSENSE_SENSOR := 60xx
 endif
 endif
+endif
+
+ifneq ($(filter 60xx 6515 65xx,$(TARGET_INVENSENSE_SENSOR)),)
+include $(call all-named-subdir-makefiles,$(TARGET_INVENSENSE_SENSOR))
 endif
