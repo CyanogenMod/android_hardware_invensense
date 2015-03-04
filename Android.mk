f5f584ee173faef40f226c6e0e8580a2ecbe079b
Ying Wang <wangying@google.com>
2015-03-03 17:15:16 -0800

    Remove files for unsupported devices.
    manta/grouper/mako are unsupported in MNC.
    Bug: 19548232
    
    Change-Id: Iebe443518b53e0a9ee10f4ed4ddc31ad984b9510

--- Android.mk
+++ Android.mk
@@ -2,14 +2,7 @@
 ifneq ($(filter hammerhead, $(TARGET_DEVICE)),)
 # hammerhead expects 65xx sensors.
 include $(call all-named-subdir-makefiles,65xx)
-else
-ifneq ($(filter guppy dory, $(TARGET_DEVICE)),)
+else ifneq ($(filter guppy dory, $(TARGET_DEVICE)),)
 # dory and guppy expect 6515 sensors.
 include $(call all-named-subdir-makefiles,6515)
-else
-ifneq ($(filter manta grouper tuna mako, $(TARGET_DEVICE)),)
-# manta, grouper, tuna, and mako expect 60xx sensors.
-include $(call all-named-subdir-makefiles,60xx)
-endif
-endif
 endif
