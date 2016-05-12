98cc547ac1d90ceab8b67ba6c05277b001b40418
sundarajan srinivasan <ssrinivasan@cyngn.com>
2016-05-12 12:04:24 -0700

    invensense: Use sensor HAL only if device specific HAL is not present
    Use sensor HAL only if device specific sensor HAL is not used.
    
    Change-Id: Iecf75af0110032742227cc062265d287dbead672

--- Android.mk
+++ Android.mk
@@ -1,4 +1,5 @@
 # Can't have both 65xx and 60xx sensors.
+ifneq ($(strip $(USE_DEVICE_SPECIFIC_SENSORS)),true)
 ifneq ($(filter hammerhead, $(TARGET_DEVICE)),)
 # hammerhead expects 65xx sensors.
 include $(call all-named-subdir-makefiles,65xx)
@@ -10,6 +11,7 @@ else
 ifneq ($(filter manta grouper tuna mako, $(TARGET_DEVICE)),)
 # manta, grouper, tuna, and mako expect 60xx sensors.
 include $(call all-named-subdir-makefiles,60xx)
+endif
 endif
 endif
 endif
